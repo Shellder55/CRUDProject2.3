66f7f5da3d35fc084a44354abe413a738bbf91a8
Shellder55 <thetwillfeed@gmail.com>
2023-11-09 22:46:52 +0600

    CRUD + Security + Spring Boot

--- src/main/java/crud/config/WebConfig.java
+++ src/main/java/crud/config/WebConfig.java
@@ -1,6 +1,7 @@
 package crud.config;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
