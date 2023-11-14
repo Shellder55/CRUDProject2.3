313f031c8f45a446709260c1ef57cceeb4a6e0cb
Shellder55 <thetwillfeed@gmail.com>
2023-11-14 17:08:48 +0600

    Spring Boot v2

--- src/main/java/crud/dao/UserDaoImpl.java
+++ src/main/java/crud/dao/UserDaoImpl.java
@@ -7,12 +7,10 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
-import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Set;
 
 @Repository
-@Transactional
 public class UserDaoImpl implements UserDao {
 
     @PersistenceContext
