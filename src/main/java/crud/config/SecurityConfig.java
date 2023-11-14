a49c937df4400c89e044c27338b4a525ccf1fd9d
Shellder55 <thetwillfeed@gmail.com>
2023-11-14 15:55:59 +0600

    Spring Boot

--- src/main/java/crud/config/SecurityConfig.java
+++ src/main/java/crud/config/SecurityConfig.java
@@ -26,15 +26,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin()
-                // указываем страницу с формой логина
-                .loginPage("/login")
                 //указываем логику обработки при логине
                 .successHandler(new LoginSuccessHandler())
-                // указываем action с формы логина
-                .loginProcessingUrl("/login")
-                // Указываем параметры логина и пароля с формы логина
-                .usernameParameter("l_username")
-                .passwordParameter("l_password")
                 // даем доступ к форме логина всем
                 .permitAll();
 
@@ -60,7 +53,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public static PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
--- src/main/java/crud/config/handler/LoginSuccessHandler.java
+++ src/main/java/crud/config/handler/LoginSuccessHandler.java
@@ -5,7 +5,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -15,7 +14,7 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
 
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest,
                                         HttpServletResponse httpServletResponse,
-                                        Authentication authentication) throws IOException, ServletException {
+                                        Authentication authentication) throws IOException {
         if (authentication.getAuthorities().contains(Role.ADMIN)) {
             httpServletResponse.sendRedirect("/admin");
         } else {
